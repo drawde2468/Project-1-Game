b1923d47c763169d6ce2299356d85f55dc203442
Edward Leslie <edwardleslie09@gmail.com>
2018-05-11 12:48:24 -0400

    completed image src read into tiles, push tile position into array of objects, and successfully recreated image with the array positions.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ let difficulty = 3;
 let myImage;
 let tileWidth;
 let tileHeight;
-let testTile;
-let imgArr = [];
+let tilesArr = [];
 
 window.onload = function() {
   createImg();
@@ -17,22 +16,79 @@ function createImg() {
     tileWidth = Math.floor(myImage.width / difficulty);
     tileHeight = Math.floor(myImage.height / difficulty);
     drawCanvas();
+    drawCanvasTest();
   };
 }
 
 function drawCanvas() {
   let canvas = document.getElementById("game-area");
   let ctx = canvas.getContext("2d");
+  let currentX = 0;
+  let currentY = 0;
 
-  ctx.drawImage(
-    myImage,
-    0,
-    0,
-    tileWidth,
-    tileHeight,
-    0,
-    0,
-    tileWidth,
-    tileHeight
-  );
+  for (i = 0; i < difficulty; i++) {
+    ctx.drawImage(
+      myImage,
+      currentX,
+      currentY,
+      tileWidth,
+      tileHeight,
+      currentX,
+      currentY,
+      tileWidth,
+      tileHeight
+    );
+
+    for (j = 0; j < difficulty; j++) {
+      ctx.drawImage(
+        myImage,
+        currentX,
+        currentY,
+        tileWidth,
+        tileHeight,
+        currentX,
+        currentY,
+        tileWidth,
+        tileHeight
+      );
+      tilesArr.push({ positionX: currentX, positionY: currentY });
+
+      ctx.strokeRect(currentX, currentY, tileWidth, tileHeight);
+      console.log("CURRENT X", currentX);
+      console.log("CURRENT Y", currentY);
+      currentX += tileWidth;
+
+      if (currentX === difficulty * tileWidth) {
+        currentX = 0;
+        currentY += tileHeight;
+      }
+    }
+  }
+  console.log(tilesArr);
+}
+
+function drawCanvasTest() {
+  let canvas = document.getElementById("game-test");
+  let ctx = canvas.getContext("2d");
+
+  for (i = 0; i < difficulty * difficulty; i++) {
+    ctx.drawImage(
+      myImage,
+      tilesArr[i].positionX,
+      tilesArr[i].positionY,
+      tileWidth,
+      tileHeight,
+      tilesArr[i].positionX,
+      tilesArr[i].positionY,
+      tileWidth,
+      tileHeight
+    );
+    ctx.strokeRect(
+      tilesArr[i].positionX,
+      tilesArr[i].positionY,
+      tileWidth,
+      tileHeight
+    );
+  }
+  console.log(tilesArr);
 }
